ce5b623ab243f0b3be5488a9fefd9b7070b0ea02
cosmii02 <cosmii02@cosmii02.com>
2023-03-13 01:18:00 +0000

    yoppi pls

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
 async function run() {
   try {
     const token = core.getInput('token');
     const octokit = github.getOctokit(token);
-    const { data: releases } = await octokit.rest.repos.listReleases({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-    });
-    
-    // Get the previous release
-    const prevRelease = releases.find(release => release.id === github.context.payload.release.id - 1);
-    
-    // Check if the previous release is a pre-release
-    if (prevRelease && prevRelease.prerelease) {
-      // Delete the previous release
-      await octokit.rest.repos.deleteRelease({
+    const eventName = github.context.eventName;
+
+    if (eventName === 'release') {
+      const { data: releases } = await octokit.rest.repos.listReleases({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
-        release_id: prevRelease.id,
       });
-      core.setOutput('deleted', true);
+
+      // Get the previous release
+      const prevRelease = releases.find(release => release.id === github.context.payload.release.id - 1);
+
+      // Check if the previous release is a pre-release
+      if (prevRelease && prevRelease.prerelease) {
+        // Delete the previous release
+        await octokit.rest.repos.deleteRelease({
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo,
+          release_id: prevRelease.id,
+        });
+        core.setOutput('deleted', true);
+      } else {
+        core.setOutput('deleted', false);
+      }
     } else {
-      core.setOutput('deleted', false);
+      core.setFailed('This action can only be triggered by a release event.');
     }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
-
-run();
-
